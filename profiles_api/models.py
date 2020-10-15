5cd1bb854a33fde918c73ced34c2714f37f6285b
Seppe Van de Poel <seppe.vandepoel@student.kdg.be>
2020-10-15 13:12:29 +0200

    Codium commit

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
+from django.conf import settings
 
 
 class UserProfileManager(BaseUserManager):
@@ -56,4 +57,16 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
         """return string representation of our user"""
         return self.email
 
-# Create your models here.
+
+class ProfileFeedItem(models.Model):
+    """profiles status update"""
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    status_text = models.CharField(max_length=255)
+    created_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        """return the model as a string"""
+        return self.status_text
